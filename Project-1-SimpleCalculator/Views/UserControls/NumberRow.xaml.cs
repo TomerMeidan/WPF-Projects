4dd56036d221e32b576e5a89a8740defda143798
Tomer Meidan <tommymeidan@gmail.com>
2024-06-19 11:49:26 +0300

    Add project files.

---
+++ Project-1-SimpleCalculator/Views/UserControls/NumberRow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+namespace Project_1_SimpleCalculator.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for NumberRow.xaml
+    /// </summary>
+    public partial class NumberRow : UserControl
+    {
+        private int _firstNumber;
+
+        public int FirstNumber
+        {
+            get { return _firstNumber; }
+            set { 
+                _firstNumber = value;
+
+                if (_firstNumber < 0)
+                    btnFirst.Visibility = Visibility.Hidden;
+                else
+                // Do not do this at all
+                btnFirst.Content = _firstNumber;
+            }
+        }
+
+        private int _secondNumber;
+
+        public int SecondNumber
+        {
+            get { return _secondNumber; }
+            set
+            {
+                _secondNumber = value;
+
+                if (_secondNumber < 0)
+                    btnSecond.Visibility = Visibility.Hidden;
+                else
+                // Do not do this at all
+                btnSecond.Content = SecondNumber;
+            }
+        }
+
+        private int _thirdNumber;
+
+        public int ThirdNumber
+        {
+            get { return _thirdNumber; }
+            set
+            {
+                _thirdNumber = value;
+
+                if (_thirdNumber < 0)
+                    btnThird.Visibility = Visibility.Hidden;
+                else
+                // Do not do this at all
+                btnThird.Content = _thirdNumber;
+            }
+            }
+
+        public NumberRow()
+        {
+            InitializeComponent();
+        }
+    }
+}
